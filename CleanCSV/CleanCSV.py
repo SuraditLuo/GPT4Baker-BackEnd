0fe7ca0c80802ca0bbbe06590cbc49a2b0fca10b
Suradit Luo <suradit_luo@cmu.ac.th>
2023-06-03 23:05:17 +0700

    Clean database and extract keyword
    Run the program manually, remove symbol, emoji, and extract keyword

---
+++ CleanCSV/CleanCSV.py
@@ -0,0 +1,131 @@
+import time
+import googletrans
+import pandas as pd
+import ast
+from Dictionary import subdistricts_chiang_mai_th as subd_cm_th
+from Dictionary import subdistricts_chiang_mai_en as subd_cm_en
+from Dictionary import bakery_list as bakery_list
+from Dictionary import review_th_positive as th_positive
+from Dictionary import review_th_negative as th_negative
+from Dictionary import review_en_positive as en_positive
+from Dictionary import review_en_negative as en_negative
+
+import demoji
+import re
+translator = googletrans.Translator()
+
+def remove_word(text):
+    text = text.replace('ตำบล', ' ')
+    text = text.replace('อำเภอ', ' ')
+    text = text.replace('ซอย', ' ')
+    text = text.replace('ถนน', ' ')
+    text = text.replace('และ', ' ')
+    text = text.replace('หรือ', ' ')
+    text = text.replace(',', ' ')
+    return text
+
+
+def get_and_clean_address_data():
+    df = pd.read_csv('../untranslated_bakery.csv', encoding='utf-8')
+    for index, row in df.iterrows():
+        district_found = []
+        thai_address = row['address']
+        thai_address = remove_word(thai_address)
+        for i in range(len(subd_cm_th)):
+            if subd_cm_th[i] in thai_address:
+                district_found.append(subd_cm_en[i])
+        print(thai_address)
+        print('-> ', district_found)
+        if len(district_found) < 1:
+            df.at[index, 'address'] = ['Other']
+        else:
+            df.at[index, 'address'] = district_found
+    df.to_csv('../translated_bakery_address.csv', index=False)
+
+def get_and_translate_menu_data():
+    df = pd.read_csv('../untranslated_bakery.csv', encoding='utf-8')
+    for index, row in df.iterrows():
+        thai_menu = row['menu']
+        if type(thai_menu) is not float:
+            #Convert sting type to list type
+            thai_menu = ast.literal_eval(thai_menu)
+            updated_thai_menu = [word.replace('\n', '') for word in thai_menu]
+            updated_thai_menu = [re.sub(r'[0-9!?,฿]', '', word) for word in updated_thai_menu]
+            updated_thai_menu = [demoji.replace(word, '') for word in updated_thai_menu]
+        else:
+            updated_thai_menu = ['N/A']
+        eng_menu = []
+        for item in updated_thai_menu:
+            while True:
+                try:
+                    translation = translator.translate(item, dest='en')
+                    eng_menu.append(translation.text)
+                    break
+                except TypeError:
+                    print("TypeError occurred. Retrying in 2 seconds...")
+                    time.sleep(1)
+        eng_menu = [word.lower() for word in eng_menu]
+        print(eng_menu)
+        df.at[index, 'menu'] = eng_menu
+    df.to_csv('../translated_bakery_menu.csv', index=False)
+
+def get_and_extract_menu_data():
+    df = pd.read_csv('../translated_bakery_menu.csv', encoding='utf-8')
+    print(bakery_list)
+    for index, row in df.iterrows():
+        menu_found = []
+        menu_list = row['menu']
+        menu_list = ast.literal_eval(menu_list)
+        for menu in menu_list:
+            for i in range(len(bakery_list)):
+                if bakery_list[i] in menu:
+                    menu_found.append(bakery_list[i])
+        print(menu_list)
+        # remove duplicated item
+        menu_found = list(set(menu_found))
+        print(len(menu_found))
+        print('-> ', menu_found)
+        if len(menu_found) < 1:
+            df.at[index, 'menu'] = ['No bakery related menu']
+        else:
+            df.at[index, 'menu'] = menu_found
+    df.to_csv('../translated_and_extract_bakery_menu.csv', index=False)
+
+def get_and_extract_review_data():
+    df = pd.read_csv('../untranslated_bakery.csv', encoding='utf-8')
+    for index, row in df.iterrows():
+        thai_reviews = row['review']
+        eng_reviews = []
+        if type(thai_reviews) is not float:
+            thai_reviews = ast.literal_eval(thai_reviews)
+            for reviews in thai_reviews:
+                reviews = remove_word(reviews)
+                review_split = re.split(r'\s+(?=[ก-ฮเ-์]+)', reviews)
+                for review in review_split:
+                    for i in range (len(th_positive)):
+                        if th_positive[i] in review:
+                            if th_negative[i] in review:
+                                eng_reviews.append(en_negative[i])
+                            else:
+                                eng_reviews.append(en_positive[i])
+        else:
+            eng_reviews = ['no review']
+        print(list(set(eng_reviews)))
+        df.at[index, 'review'] = list(set(eng_reviews))
+    df.to_csv('../extracted_bakery_review.csv', index=False)
+def combined_and_create_csv():
+    df = pd.read_csv('../untranslated_bakery.csv', encoding='utf-8')
+    address_df = pd.read_csv('../translated_bakery_address.csv', encoding='utf-8')
+    menu_df = pd.read_csv('../translated_and_extract_bakery_menu.csv', encoding='utf-8')
+    review_df = pd.read_csv('../extracted_bakery_review.csv', encoding='utf-8')
+    df['address'] = address_df['address']
+    df['menu'] = menu_df['menu']
+    df['review'] = review_df['review']
+    df.to_csv('../cleaned_bakery.csv', index=False)
+if __name__ == '__main__':
+    # get_and_clean_address_data()
+    # get_and_clean_menu_data()
+    # get_and_translate_menu_data()
+    # get_and_extract_menu_data()
+    # get_and_extract_review_data()
+    combined_and_create_csv()
---
+++ CleanCSV/Dictionary.py
@@ -0,0 +1,32 @@
+subdistricts_chiang_mai_th = ["ช้างคลาน", "ช้างม่อย", "ช้างเผือก", "ช้างวัฒนา", "เชียงดาว", "จอมทอง", "ฟ้าฮ่าม", "ฝาง",
+                              "หางดง", "ห้วยทราย", "ห้วยแก้ว", "แม่อาย", "แม่เหียะ", "แม่ปิง", "แม่ริม", "แม่แตง", "แม่ออน", "เมืองเชียงใหม่", "เมือง", "หนองหอย",
+                              "หนองป่าครั่ง","ป่าแดด", "สันกำแพง", "สันกลาง", "สันทราย", "สันทรายหลวง", "สารภี", "สบแม่ข่า", "ศรีภูมิ",
+                              "สุเทพ", "ตลาดขวัญ", "ท่าศาลา", "ท่าวังตาล", "ต้นเปา", "เชียงใหม่บิสสิเนสพาร์ค", "นิมมานเหมินท์", "นิมมานเหมินทร์", "สันป่าตอง",
+                              "เจริญราษฎร์", "เวียงบัว", "ราชพฤกษ์", "ท่าแพ", "พระปกเกล้า", "ราชวิถี", "โชตนา", "ศิริมังคลาจารย์"]
+subdistricts_chiang_mai_en = ["Chang Khlan", "Chang Moi", "Chang Phueak", "Chang Watthana", "Chiang Dao", "Chom Thong",
+                              "Fa Ham", "Fang", "Hang Dong", "Huai Sai", "Huai Kaew", "Mae Ai", "Mae Hia",
+                              "Mae Ping", "Mae Rim", "Mae Taeng", "Mae On", "Mueang Chiang Mai", "Mueang Chiang Mai", "Nong Hoi",
+                              "Nong Pa Khrang", "Pa Daet", "San Kamphaeng", "San Klang", "San Sai", "San Sai Luang", "Saraphi",
+                              "Sop Mae Kha", "Sri Phum", "Suthep", "Talat Khwan", "Tha Sala", "Tha Wang Tan", "Ton Pao",
+                              "Chiang Mai business park", "Nimmanhaemin", "Nimmanhaemin", "San Pa Tong", "Charoenrat Rd.", "Wiang Bua Rd.",
+                              "Ratchaphruek Rd.", "Tha Phae Rd.", "Prapokkloa Rd.", "Rajavithi Rd.", "Chotana Rd.", "Sirimangkalachan Rd."]
+bakery_list = [
+    "bread", "croissant", "baguette", "pastry", "cake", "cupcake", "muffin", "donut", "cookie", "brownie", "pie", "pretzel",
+    "scone", "roll", "bun", "danish", "strudel", "tart", "éclair", "macaron", "cinnamon roll", "cheesecake", "fruitcake", "pound cake",
+    "shortbread", "biscuit", "mille-feuille", "pain au chocolat", "palmier", "pita bread", "pizza dough", "quiche", "sourdough",
+    "brioche", "focaccia", "pita", "naan", "crostini", "bagel"]
+
+review_th_positive = ['อร่อย', 'สด', 'หอม', 'นุ่ม', 'คุ้มค่า', 'บรรยากาศดี', 'บริการดี', 'ราคาถูก', 'คุณภาพดี', 'พนักงานบริการดี', 'อบอุ่น',
+                      'เป็นกันเอง', 'เพลิดเพลิน', 'สะอาด', 'ใส่ใจ', 'ชอบ', 'สวย', 'น่ารัก', 'คุ้มราคา', 'แนะนำ', 'ราคาสมเหตุสมผล',
+                      'หน้าตาดี', 'เอาใจใส่']
+review_th_negative = ['ไม่อร่อย', 'ไม่สด', 'ไม่หอม', 'ไม่นุ่ม', 'ไม่คุ้มค่า', 'ไม่บรรยากาศดี', 'ไม่บริการดี', 'ไม่ราคาถูก', 'ไม่คุณภาพดี',
+                      'พนักงานบริการไม่ดี', 'ไม่อบอุ่น', 'ไม่เป็นกันเอง', 'ไม่เพลิดเพลิน', 'ไม่สะอาด', 'ไม่ใส่ใจ', 'ไม่ชอบ', 'ไม่สวย',
+                      'ไม่น่ารัก', 'ไม่คุ้มราคา', 'ไม่แนะนำ', 'ไม่ราคาสมเหตุสมผล', 'ไม่หน้าตาดี', 'ไม่เอาใจใส่']
+review_en_positive = ['delicious', 'fresh', 'fragrant', 'soft', 'worth it', 'good atmosphere', 'good service', 'affordable',
+                      'good quality', 'excellent service', 'warm', 'tasty', 'authentic', 'enjoyable', 'clean', 'attentive',
+                      'lovely', 'beautiful', 'adorable', 'value for money', 'recommended', 'reasonable price', 'nice-looking',
+                      'thoughtful']
+review_en_negative = ['not tasty', 'not fresh', 'not fragrant', 'tough', 'not worth', 'terrible atmosphere', 'bad service',
+                      'unaffordable', 'bad quality', 'bad service', 'unpleasant', 'unauthentic', 'not enjoyable', 'dirty'
+                      'inattentive', 'unlovely', 'ugly', 'not cute', 'not worth', 'not recommend', 'not reasonable price',
+                      'uncool-looking', 'unthoughtful']
